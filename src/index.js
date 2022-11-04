9a828cf26e57cbc63be4a806df9ae241f3e70af8
oeyoews <jyao4783@gmail.com>
2022-11-04 16:50:16 +0800

    Automatic commit

--- src/index.js
+++ src/index.js
@@ -7,15 +7,8 @@ if (require("electron-squirrel-startup")) {
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     // frame: false,
-    // transparent: true,
-    // backgroundColor: "#fff", // 窗口的背景颜色为十六进制值
-    // titleBarStyle: "hidden", //窗口标题栏的样式 (no support linux)
-    //网页功能的设置
-    // devTools: true, //是否开启 DevTools
-    // webSecurity: false//是否禁用同源策略
     width: 1200,
     height: 800,
     center: true, // 是否出现在屏幕居中的位置
@@ -24,7 +17,6 @@ const createWindow = () => {
     resizable: true, //窗口是否可以改变尺寸
     autoHideMenuBar: true, //是否隐藏菜单栏
     webPreferences: {
-      // preload: path.join(__dirname, "preload.js"),
       // preload: path.join(__dirname, "notify.js"),
     },
   });
@@ -41,6 +33,14 @@ const createWindow = () => {
 
   const trayMenuTemplate = [
     {
+      label: " about",
+      click: () => { },
+    },
+    {
+      label: " setup",
+      click: () => { },
+    },
+    {
       label: " open",
       click: () => {
         mainWindow.show();
@@ -49,7 +49,7 @@ const createWindow = () => {
     {
       label: " exit",
       click: () => {
-        app.quit();
+        // app.quit();
         app.quit(); //因为程序设定关闭为最小化，所以调用两次关闭，防止最大化时一次不能关闭的情况
       },
     },
@@ -57,7 +57,8 @@ const createWindow = () => {
 
   function traysetup() {
     let tray = null;
-    tray = new Tray("static/images/icon.ico");
+    // tray = new Tray("static/images/icon.ico");
+    tray = new Tray(path.join(__dirname, "/static/images/icon.ico"));
     const contextMenu = Menu.buildFromTemplate(trayMenuTemplate);
     tray.setContextMenu(contextMenu);
   }
@@ -67,23 +68,6 @@ const createWindow = () => {
   // and load the index.html of the app.
   // mainWindow.loadURL("https://bing.com");
   mainWindow.loadFile(path.join(__dirname, "index.html"));
-
-  const INCREMENT = 0.03;
-  const INTERVAL_DELAY = 100; // ms
-
-  /* let c = 0;
-  progressInterval = setInterval(() => {
-    // update progress bar to next value
-    // values between 0 and 1 will show progress, >1 will show indeterminate or stick at 100%
-    mainWindow.setProgressBar(c);
-
-    // increment or reset progress bar
-    if (c < 2) {
-      c += INCREMENT;
-    } else {
-      c = -INCREMENT * 5; // reset to a bit less than 0 to show reset state
-    }
-  }, INTERVAL_DELAY); */
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
@@ -110,6 +94,3 @@ app.on("activate", () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
